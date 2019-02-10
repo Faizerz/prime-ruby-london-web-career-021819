bfce47a22de6dd4906d40207fb26bb3e5a281ad2
Faiz Abbas <faiz_abbas@hotmail.co.uk>
2019-02-10 14:58:55 +0000

    Automatically backed up by Learn

--- prime.rb
+++ prime.rb
@@ -2,8 +2,8 @@ def prime?(n)
   if n<0 || n==1 || n==0
     return FALSE
   else  
-    ary = new.
-    Array(2..n-1).each do |x|
+    ary = new.Array(2..n-1)
+    .each do |x|
        if n % x != 0
          return TRUE
        else FALSE
