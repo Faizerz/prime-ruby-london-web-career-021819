96916368cc33c6ca708a4cb08f321bd4ff137a22
Faiz Abbas <faiz_abbas@hotmail.co.uk>
2019-02-10 14:38:48 +0000

    Automatically backed up by Learn

--- prime.rb
+++ prime.rb
@@ -1,1 +1,8 @@
 # Add  code here!
+def prime?(n)
+# Makes a range of numbers from 2 to n
+ ary = [2..n];
+# Loops through each and divides n by each
+# If % true then not-prime else is prime
+end
+  
