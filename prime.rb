45dda13e5f37cf1d632b577293dddc138ab56c21
Faiz Abbas <faiz_abbas@hotmail.co.uk>
2019-02-10 14:39:39 +0000

    Automatically backed up by Learn

--- prime.rb
+++ prime.rb
@@ -1,7 +1,7 @@
 # Add  code here!
 def prime?(n)
 # Makes a range of numbers from 2 to n
- ary = [2..n];
+ ary = Array(2..(n-1));
 # Loops through each and divides n by each
 # If % true then not-prime else is prime
 end
