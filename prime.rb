190de985116c12ffe1a3bd4a0267c0e6c9398a77
Faiz Abbas <faiz_abbas@hotmail.co.uk>
2019-02-10 14:45:25 +0000

    Automatically backed up by Learn

--- prime.rb
+++ prime.rb
@@ -1,11 +1,12 @@
 # Add  code here!
 def prime?(n)
-# Makes a range of numbers from 2 to n
+
  ary = Array(2..(n-1));
-# Loops through each and divides n by each
+
   ary.each do |x|
     result = n/|x|
-# If integer then not prime
+
   result.is_a? Integer
+end
 end
   
