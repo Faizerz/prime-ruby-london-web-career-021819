ffeeeb813ee0fb18229ce8e37e046470b19a955f
Faiz Abbas <faiz_abbas@hotmail.co.uk>
2019-02-10 15:02:33 +0000

    Automatically backed up by Learn

--- prime.rb
+++ prime.rb
@@ -2,11 +2,8 @@ def prime?(n)
   if n < 0 or n == 0 or n == 1
     return false
   else
-    (2..n-1).to_a.each do |x|
-    if n % x != 0 
-      return true
-    else
-      return false
+    (2..n-1).to_a.all? do |x|
+      n % x != 0
     end
   end
 end
